b44de9e7ff0eea6d077cdc8cb6b98155d0ecc7ad
thejaustin <thejaustin@users.noreply.github.com>
2026-08-07 08:51:03 -0500

    chore: Bump version to v1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.thejaustin.simweather"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.7.0"
+        versionCode = 9
+        versionName = "1.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
