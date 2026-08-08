425d1c76f996706db43cddbf9e4e2a29c9b5acfe
thejaustin <thejaustin@users.noreply.github.com>
2026-08-08 00:15:05 -0500

    feat: SimWeather v2.1.0 Component Textures & debroid Autonomous Debugger Skill

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.thejaustin.simweather"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.0.0"
+        versionCode = 11
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
